fc6bd0b89ba5d0e1c203bf92fadce91a7c9149ea
Tshepang <tshepang@webtonic.co.za>
2025-08-17 11:08:23 +0200

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
+  server: {
+    host: true,
+    port: 3000
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
